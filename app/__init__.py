5c838d5101dc2686aafb55e5dd168ffed33acba8
Nick Morris <nicholasmorris2599@gmail.com>
2023-05-03 13:17:11 -0700

    Commented out previous code for reference and setting up initial starting test route returning json format.

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,32 @@
-from app.routes import home, dashboard, api
-from app.db import init_db
-from app.utils import filters
+# from app.routes import home, dashboard, api
+# from app.db import init_db
+# from app.utils import filters
 from flask import Flask
 
-def create_app(test_config=None):
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+# def create_app(test_config=None):
+  # app = Flask(__name__, static_url_path='/')
+  # app.url_map.strict_slashes = False
+  # app.config.from_mapping(
+  #   SECRET_KEY='super_secret_key'
+  # )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
+
+  # app.register_blueprint(home)
+  # app.register_blueprint(dashboard)
+  # app.register_blueprint(api)
+
+  # app.jinja_env.filters['format_url'] = filters.format_url
+  # app.jinja_env.filters['format_date'] = filters.format_date
+  # app.jinja_env.filters['format_plural'] = filters.format_plural
+
+  # init_db(app)
+
+  # return app
+app = Flask(__name__, static_url_path='/')
+
+@app.route('/tests')
+def test():
+  return {"tests":["Test1", "Test2", "Test3"]}
   
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
-  app.register_blueprint(api)
-
-  app.jinja_env.filters['format_url'] = filters.format_url
-  app.jinja_env.filters['format_date'] = filters.format_date
-  app.jinja_env.filters['format_plural'] = filters.format_plural
-
-  init_db(app)
-
-  return app
+if __name__ == "__main__":
+  app.run(debug=True)
